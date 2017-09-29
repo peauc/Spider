0b77a57feae78d38ce33c3cd90ba7e95c2f68706
Axel Drozdzynski <axeldroz@hotmail.fr>
2017-09-29 14:50:23 +0200

    Add IListener.hh

---
+++ src/IListener.hh
@@ -0,0 +1,21 @@
+//
+// IListener.hh for IListener in c:\Users\49207\Documents\Epitech-Rendu\cpp_spider
+//
+// Made by Axel Drozdzynski
+// Login   <drozdz_b>
+//
+// Started on  Sat Sep 29 14:27:00 2017 Axel Drozdzynski
+// Last update Sat Sep 29 14:48:55 2017 Axel Drozdzynski
+//
+
+#ifndef ILISTENER_HH_
+# define ILISTENER_HH_
+
+class IListener
+{
+private:
+    ~IListener();
+    virtual void run() = 0;
+}
+
+#endif /* !ILISTENER_HH_ */
