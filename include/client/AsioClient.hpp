b9909620f8566036b39dc2a93387491a7a9eeafb
alexandre thauvin <alexandre.thauvin@epitech.eu>
2017-10-06 13:12:17 +0200

    streambuf to string + mise en forme handle

---
+++ include/client/AsioClient.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Clément Péau on 27/09/2017.
+//
+
+#ifndef CPP_SPIDER_ASIOCLIENT_HPP
+#define CPP_SPIDER_ASIOCLIENT_HPP
+
+#include "AClient.hpp"
+#include <boost/asio.hpp>
+#include <iostream>
+#include "Module.h"
+
+class AsioClient : public AClient {
+public:
+	AsioClient(boost::asio::io_service &, unsigned short);
+	~AsioClient();
+  void	try_send(const std::string);
+  void 	handle_read_state(const boost::system::error_code&);
+  void 	handle_read_h(const boost::system::error_code&);
+  void 	handle_read_body(const boost::system::error_code&);
+  bool 	check_exit(const std::string) const;
+ private:
+  unsigned short			port;
+  boost::asio::ip::tcp::socket 		socket;
+  boost::asio::streambuf 		answer;
+  //std::map<char, Module>		modules;
+};
+
+#endif //CPP_SPIDER_ASIOCLIENT_HPP
