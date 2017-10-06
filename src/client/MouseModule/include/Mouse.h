6744a104a0a4621d5a2d7ac25512b53690acd4c2
Benjamin <benjamin.quastana@epitech.eu>
2017-10-06 17:08:01 +0200

    MouseListener

---
+++ src/client/MouseModule/include/Mouse.h
@@ -0,0 +1,39 @@
+//
+// Created by Quastana Benjamin on 05/10/2017.
+//
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#ifndef TESTMOUSE_MOUSE_H_H
+#define TESTMOUSE_MOUSE_H_H
+
+#include "IListener.hh"
+
+struct         s_mouseData
+{
+    int            timestamp;
+    int            key_code;
+    int            x;
+    int            y;
+    struct    s_mouseData    *next;
+};
+
+class MouseClickListener : public IListener
+{
+public:
+    MouseClickListener();
+    ~MouseClickListener();
+    void    GetMousePos();
+    virtual int run();
+    int                 runThread();
+    virtual int stop();
+
+private:
+    s_mouseData     mouse;
+    bool            cont;
+    std::ofstream   fichier;
+};
+
+#endif //TESTMOUSE_MOUSE_H_H
+
+#pragma clang diagnostic pop
